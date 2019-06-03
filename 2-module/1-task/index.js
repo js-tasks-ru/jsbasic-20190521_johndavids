22ace5ccf4d31eae1b57ea9571c6c9c617c00184
John Davids <johndavids89308@gmail.com>
2019-06-03 15:27:29 +0400

    CloneObj

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,15 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
 function clone (obj) {
+
+    let clons = {};
+
+
+    for (let key in obj) {
+        clons[key] = obj[key];
+    }
+    return obj;
+
 }
