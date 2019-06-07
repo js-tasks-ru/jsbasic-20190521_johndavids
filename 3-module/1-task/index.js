e5a285d439e0d9725645ba7cca06d1454f627abc
John Davids <johndavids89308@gmail.com>
2019-06-07 16:44:39 +0400

    Функция showSalary

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,12 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(data, age) {
-}
 
+function showSalary(data, age) {
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].age <= age){
+            console.log(`${data[i].name}, ${data[i].balance}`);
+        }
+    }
+}
 
