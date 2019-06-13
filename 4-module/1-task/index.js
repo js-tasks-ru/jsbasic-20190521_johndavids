409d4f6d7539c158a567f04a464aca16abd89dd1
John Davids <johndavids89308@gmail.com>
2019-06-13 23:24:18 +0400

    Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,19 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+	let ul = document.createElement('ul');
+	let li;
+	let res;
+	document.body.appendChild(ul);
+
+	for(let i=0; i < friends.length; i++){
+
+		li = document.createElement('li');
+		li.innerHTML = friends[i]['firstName'] + ' ' + friends[i]['lastName'];
+		document.body.getElementsByTagName('ul')[0].appendChild(li);
+
+	}
+
+	return res = document.body.getElementsByTagName('ul')[0];
+
 }
