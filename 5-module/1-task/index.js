ff24f1476267a745d963fce121540d64a95da918
John Davids <johndavids89308@gmail.com>
2019-06-17 22:54:12 +0400

    Функция highlight

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,47 @@
  * @param {Element} table
  */
 function highlight(table) {
+	let tr = document.getElementsByTagName('tr');
+
+	
+	for(let i = 1; i < tr.length; i++){
+
+		// Start - Проставит в "<tr>" класс male/female, в зависимости от содержимого ячекйки Gender
+
+		if(tr[i].children[2].textContent === 'm'){
+			
+			tr[i].classList.add("male");
+
+		}else if (tr[i].children[2].textContent === 'f'){
+
+			tr[i].classList.add("female");
+
+		}
+
+		// End - Проставит в "<tr>" класс male/female, в зависимости от содержимого ячекйки Gender
+
+		// Start - Проставит класс available/unavailable, в зависимости от значения атрибута data-available у ячейки Status
+
+		if(tr[i].children[3].getAttribute("data-available") === "true"){
+			tr[i].classList.add("available");
+			//console.log(tr[i].children[3].getAttribute("data-available"));
+		}else if(tr[i].children[3].getAttribute("data-available") === "false"){
+			tr[i].classList.add("unavailable");
+		}else{
+			tr[i].setAttribute("hidden", 'true');
+		}
+		
+		// End - Проставит класс available/unavailable, в зависимости от значения атрибута data-available у ячейки Status
+
+		// Start - Установит inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18
+
+		if(tr[i].children[1].textContent < 18){
+			tr[i].style = "text-decoration: line-through";
+		}
+
+		// End - Установит inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18
+
+
+	}
+
 }
